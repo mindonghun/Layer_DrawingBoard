907b4fb5f4884a46d3e81724dc4454b27c6832c9
mindonghun <88094304+mindonghun@users.noreply.github.com>
2022-11-12 17:07:41 +0900

    Update MainFrame.java

--- Layer_DrawingBoard_JAVA/MainFrame.java
+++ Layer_DrawingBoard_JAVA/MainFrame.java
@@ -1,19 +1,33 @@
 package Layer_DrawingBoard_JAVA;
-import javax.swing.JFrame;
-import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseMotionListener;
+
+
+
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 import javax.swing.*;
-
 
 public class MainFrame extends JFrame{
 
     JPanel main_panel;
 
+
+
     JButton pencil_btn;
     ImageIcon pencil_btn_img = new ImageIcon("./Layer_DrawingBoard_JAVA/Button_Image/pencil_btn_img.png");
 
     MainFrame(){
+        System.out.println("MainFrame_start");
+
         // 타이틀 문구 설정
         setTitle("Layer_DrawingBoard");
         // 프레임 크기 설정 및 x버튼 종료 기능 지정
@@ -22,8 +36,6 @@ public class MainFrame extends JFrame{
 
         // 패널 객체화
         main_panel = new JPanel();
-        main_panel.setBackground(Color.WHITE);
-
         
         pencil_btn = new JButton(pencil_btn_img);
         pencil_btn.setPreferredSize(new Dimension(28,28));
@@ -32,9 +44,32 @@ public class MainFrame extends JFrame{
 
         main_panel.add(pencil_btn);
 
-
+        addMouseListener(new MyMouseListener());
+        addMouseMotionListener(new MyMouseListener());
         // 메인 프레임에 패널 부착
         add(main_panel);
         setVisible(true);
     }
+    
+
+    class MyMouseListener extends MouseAdapter{
+        public void mousePressed(MouseEvent e){ // 눌린순간
+            int x = e.getX();
+            int y = e.getY();
+            System.out.println("mousePressed : Point("+x+","+y+")");
+        }   
+        public void mouseDragged(MouseEvent e){ // 드래그일시
+            int x = e.getX();
+            int y = e.getY();
+            System.out.println("mouseDragged : Point("+x+","+y+")");
+        }
+        public void mouseReleased(MouseEvent e) {
+            int x = e.getX();
+            int y = e.getY();
+            System.out.println("mouseReleased : Point("+x+","+y+")");
+        }
+    }
+
+
+
 }
