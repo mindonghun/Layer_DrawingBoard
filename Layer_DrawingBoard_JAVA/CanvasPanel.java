cfb91f767e32fb5c48151dae58804d88926e47c5
mindonghun <88094304+mindonghun@users.noreply.github.com>
2022-11-17 15:12:31 +0900

    임시 그리기

--- Layer_DrawingBoard_JAVA/CanvasPanel.java
+++ Layer_DrawingBoard_JAVA/CanvasPanel.java
@@ -14,9 +14,9 @@ import java.awt.Point;
 
 public class CanvasPanel extends JPanel{
     Dimension size;
-    Point p_s, p_e;
+    Point p_s, p_e, p_d;
     List<Command> command_list;
-
+    Command tmp_command = null;
 
     CanvasPanel(){
         System.out.println("CanvaslPanel");
@@ -35,7 +35,8 @@ public class CanvasPanel extends JPanel{
         for(int c=0; c<command_list.size(); c++){
             command_list.get(c).execute(g);
         }
-
+        if(tmp_command != null)
+            tmp_command.execute(g);
     }
 
     class MyMouseListener extends MouseInputAdapter{
@@ -50,11 +51,15 @@ public class CanvasPanel extends JPanel{
             Point point = e.getPoint();
             System.out.println("mousePressed : Point="+point);
             p_s = e.getPoint();
-
         }   
         public void mouseDragged(MouseEvent e){ // 드래그일시
             Point point = e.getPoint();
             System.out.println("mouseDragged : Point="+point);
+            p_d = e.getPoint();
+            //tmp_command = finalize();
+            tmp_command = new DrawCommand(new Line(p_s,p_d));
+            context.repaint();
+
         }
         public void mouseReleased(MouseEvent e) {
             Point point = e.getPoint();
